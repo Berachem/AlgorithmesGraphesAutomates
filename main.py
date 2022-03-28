be78cc9f905aeb847a41c739e15d0b139b17a721
Berachem Markria <berachem.markria@gmail.com>
2022-03-28 22:24:25 +0200

    DEBUT

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-print("Salut")
 
-def concatener(L1, L2):
+L1=['aa','ab','ba','bb']
+L2=['a', 'b', '']
+
+
+
+def concatene(L1, L2):
     L3 = set()
     for e1 in L1:
         for e2 in L2:
             L3.add(e1+e2)
-    return list(set)
+    return list(L3)
+
+print(concatene(L1,L2))
+
+
+# A CORRIGER
+def puis(L1,n):
+    L3 = set()
+    for e1 in L1:
+        for e2 in L1:
+            L3.add(e1+e2)
+    return list(filter(lambda s : len(s)==n,L3))
+
+L1=['aa','ab','ba','bb']
+print(puis(L1,2))
