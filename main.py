0d4db22c3ef743a5ccf896d53f06ecd0b68dd5cf
Berachem Markria <61350744+Berachem@users.noreply.github.com>
2022-03-31 09:58:37 +0200

    jusqu'à la partie 1.3 c'est fait !

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def pref(mot):
         L.append(mot[:i])
     return list(L)
 
-print(pref('coucou'))
+print("prefixe ->",pref('coucou'))
 
 def suf(mot):
     L = []
@@ -16,20 +16,23 @@ def suf(mot):
         L.append(mot[i:])
     return list(L)
 
-print(suf('coucou'))
+print("suffixe -> ",suf('coucou'))
 
 def fact(mot):
-    L = []
-    
-    return L
+    L = set()
+    for i in range(len(mot)):
+        for j in range(i,len(mot)):
+            L.add(mot[i:j+1])
+    L.add("")
+    return sorted(list(L))
 
-print(fact('coucou'))
+print("facteur -> ",fact('coucou'))
 
 
 def miroir(mot):
     return "".join(mot[::-1])
 
-print(miroir('coucou'))
+print("miroir -> ", miroir('coucou'))
 
 def concatene(L1, L2):
     L3 = set()
@@ -38,14 +41,26 @@ def concatene(L1, L2):
             L3.add(e1+e2)
     return list(L3)
 
-print(concatene(L1,L2))
+print("concatene -> ",concatene(L1,L2))
 
 
 def puis(L1,n):
-    ans = L1[:]
-    for _ in range(n - 1):
-        ans = [word + char for word in ans for char in L1]
-    return list(set(ans))
+    if n!=0:
+        ans = L1[:]
+        for _ in range(n - 1):
+            ans = [word + char for word in ans for char in L1]
+        return list(set(ans))
+    return ['']
 
 L1=['aa','ab','ba','bb']
-print(puis(L1,2))
+print("puissance (L^n) -> ",puis(L1,2))
+
+
+def tousmots(L1,n):
+    L = set()
+    for i in range(n+1):
+        L = L.union(set(puis(L1,i)))
+        
+    return list(filter(lambda mot: len(mot)<=n,L))
+
+print("tousmots -> ",tousmots(['a','b'],3))
