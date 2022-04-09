86c8629b7a0a721659a33fcbb59a083756ff1748
Berachem Markria <berachem.markria@gmail.com>
2022-04-09 11:45:30 +0200

    amélioration affichages et nom etat puis

--- main.py
+++ main.py
@@ -109,21 +109,21 @@ def lirelettre(T, E, a):
       
 print("lirelettre -> ",lirelettre(auto["transitions"],auto["etats"],'a'))
 
-"""
+
 def liremot(T,E,m):
-  En = lirelettre(T, E, a)
-  for a in m:
-    lirelettre(T, En, a)
-
-  return En
-
-		
+    new_E = E
+    for a in m:
+        new_E = lirelettre(T, new_E, a)
+    return new_E
+    
+print("----------------------------------------------")	
 print(liremot(auto["transitions"],auto["etats"],'aba'))
+print("-----------------------------------------------")
 
 
 def accepte(auto, m):
-	return len(liremot(auto["transitions"],auto["etats"],m))>0
-"""
+	return len(liremot(auto["transitions"],auto["etats"],m)) > 0
+
 	   
 	
 def langage_accept(auto, n):
@@ -159,8 +159,8 @@ auto1 ={"alphabet":['a','b'],"etats": [0,1],
 auto2={"alphabet":['a','b'],"etats": [0,1],
 "transitions":[[0,'a',0],[0,'a',1],[1,'b',1],[1,'a',1]], "I":[0],"F":[1]}
 
-print("est déterministe ? doc Test 1 ->" ,deterministe(auto0))
-print("est déterministe ? doc Test 2 ->", deterministe(auto2))
+print("est déterministe ? doc Test 1 (doit être True) ->" ,deterministe(auto0))
+print("est déterministe ? doc Test 2 (doit être False) ->", deterministe(auto2))
 
 def determinise(auto):
     I = [auto["I"]]
@@ -202,7 +202,7 @@ auto2Determinise = {'alphabet': ['a', 'b'],
 'etats': [[0], [0, 1], [1]], 
 'F': [[0, 1], [1]]}
 
-print("déterminisation doc Test-> ",determinise(auto2)==auto2Determinise)
+print("déterminisation doc Test (doit être True) -> ",determinise(auto2)==auto2Determinise)
 
 def renommage(auto):
     cpt = 0
@@ -224,4 +224,94 @@ def renommage(auto):
 
 auto2Renomme = {'alphabet': ['a', 'b'], 'etats': [0, 1, 2],'transitions': [[0, 'a', 1], [1, 'a', 1],[1, 'b', 2], [2, 'a', 2], [2, 'b', 2]],'I': [0], 'F': [1, 2]}
 
-print("renommage doc Test réussi-> ",renommage(determinise(auto2))==auto2Renomme)
+print("renommage doc Test (doit être True)-> ",renommage(determinise(auto2))==auto2Renomme)
+
+
+# Complementation 
+
+auto3 = {"alphabet":['a','b'],"etats": [0,1,2],
+"transitions":[[0,'a',1],[0,'a',0],[1,'b',2],[1,'b',1]], "I":[0],"F":[2]}
+
+def complet(auto):
+  dico_verif = {}
+  for transi in auto['transitions']:
+    if transi[0] not in dico_verif.keys():
+      dico_verif[transi[0]] = set()
+    dico_verif[transi[0]].add(transi[1])
+
+  for etat in dico_verif.keys():
+    if len(dico_verif[etat]) != len(auto['alphabet']):
+      return False
+
+  return True
+
+
+print()
+print("test complet (doit être False) -> " + str(complet(auto0)))
+print("test complet (doit être True) -> " + str(complet(auto1)))
+
+def complete(auto):
+  if complet(auto):
+    return auto
+  else:
+    transitions = auto['transitions']
+    nomEtatPuis = max(auto['etats'])+1
+    dico_verif = {}
+    for transi in auto['transitions']:
+      if transi[0] not in dico_verif.keys():
+        dico_verif[transi[0]] = set()
+        dico_verif[transi[2]] = set()
+      dico_verif[transi[0]].add(transi[1])
+
+    for etat in dico_verif.keys():
+      for lettre in auto['alphabet']:
+        if lettre not in dico_verif[etat]:
+          transitions.append([etat, lettre, nomEtatPuis])
+
+    transitions.append([nomEtatPuis, 'a', nomEtatPuis])
+    transitions.append([nomEtatPuis, 'b', nomEtatPuis])
+
+    etats = auto['etats']
+    etats.append(nomEtatPuis)
+
+    return {'alphabet': auto['alphabet'], 
+'I': auto['I'], 
+'transitions': transitions, 
+'etats': etats, 
+'F': auto['F']}
+
+print()
+print("Doc Test pour savoir si ça complète bien (doit être True) -> " ,complete(auto0)=={'alphabet': ['a', 'b'], 'etats': [0, 1, 2, 3, 4],
+'transitions': [[0, 'a', 1], [1, 'a', 1], [1, 'b', 2], [2, 'a', 3],
+[0, 'b', 4], [2, 'b', 4], [3, 'a', 4], [3, 'b', 4], [4, 'a', 4], [4, 'b', 4]],
+'I': [0], 'F': [3]})
+print("test pour savoir si c'est bien complet (doit être True) -> " + str(complet(auto0)))
+
+def complement(auto):
+  auto_new = determinise(auto)
+  auto_new = renommage(auto_new)
+  auto_new = complete(auto_new)
+  
+  new_etat = []
+  for etat in auto_new['etats']:
+    if etat not in auto_new['F']:
+        new_etat.append(etat)
+
+  auto_new['F'] = new_etat
+
+  return auto_new
+
+print()
+print("Doc Test pour savoir si le complement est bon (doit être True) -> ",complement(auto3)=={'alphabet': ['a', 'b'], 'etats': [0, 1, 2, 3],
+'transitions': [[0, 'a', 1], [1, 'a', 1], [1, 'b', 2], [2, 'b', 2],
+[0, 'b', 3], [2, 'a', 3], [3, 'a', 3], [3, 'b', 3]], 'I': [0], 'F': [0, 1, 3]}
+)
+
+# Produit
+
+auto4 ={"alphabet":['a','b'],"etats": [0,1,2,],
+"transitions":[[0,'a',1],[1,'b',2],[2,'b',2],[2,'a',2]], "I":[0],"F":[2]}
+
+auto5 ={"alphabet":['a','b'],"etats": [0,1,2],
+"transitions":[[0,'a',0],[0,'b',1],[1,'a',1],[1,'b',2],[2,'a',2],[2,'b',0]],
+"I":[0],"F":[0,1]}
